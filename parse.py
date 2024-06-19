d77f2254137ea086e57ec0da2ea51be1610aeca4
meshchellomishel <126414636+meshchellomishel@users.noreply.github.com>
2024-06-19 16:16:57 +0300

    add support of recieve messages and several messages

--- parse.py
+++ parse.py
@@ -6,17 +6,21 @@ import json
 import re
 
 
-data = ""
+data = []
+debug = False
 file2parse = "trace.log"
-output = "t.log"
-if len(sys.argv) > 1:
-    file2parse = sys.argv[1]
+output = "t.json"
 
+if '-d' in sys.argv:
+    debug = True
 
 with open(file2parse, "r") as file:
     for line in file.readlines():
-        if not "send" in line:
+        if not "send" in line and not "recv" in line:
             continue
+
+        send_check = "send" in line
+        msg = "send" if send_check else "recv"
 
         i = 0
         j = len(line) - 1
@@ -31,17 +35,18 @@ with open(file2parse, "r") as file:
             continue
 
         parse = line[i+1:j]
-        parse = processed = re.sub(r'(?<![\\\w])([a-zA-Z_\d]+[\(\<\]*\d*\)?[\|a-zA-Z_]*)', r'"\1"', parse.replace('\n', '')).replace('\\', '\\\\').replace('=', ':').replace('"true"', 'true').replace('"false"', 'false')
+        parse = processed = re.sub(r'(?<![\\\w])([a-zA-Z_\d-]+[\(\<\]*\d*\)?[\|a-zA-Z_]*)', r'"\1"',
+                                   parse.replace('\n', '')).replace('\\', '\\\\').replace('=', ':').replace('"true"', 'true').replace('"false"', 'false').replace('...', '')
         parse = "[" + parse + "]"
-        data = json.loads(parse)
-        print(data)
 
+        if debug:
+            print(parse)
+
+        data.append("{\"" + msg + "\":" + parse + "}")
+
+data = ",".join(data)
+data = "[" + data + "]"
+data = json.loads(data)
 
 with open(output, "w+") as file:
     json.dump(data, file, indent=True)
-
-
-
-            
-
-
